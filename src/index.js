e56846b21b93d28330e8a68781c578d1403858ae
ULADZIMIR MARTYNCHYK <83595316+Waldemar-00@users.noreply.github.com>
2023-07-14 21:00:55 +0300

    fetch in the useEffect and handlerClick

--- src/index.js
+++ src/index.js
@@ -4,19 +4,30 @@ import "./styles.css";
 // import getData from "./getData";
 
 function App() {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
   const [get, setGet] = useState([]);
+  const [url, setUrl] = useState("https://jsonplaceholder.typicode.com/users");
   useEffect(() => {
     document.title = "useEffect";
-  }, []);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setGet(data);
+      });
+    document.querySelector("button").addEventListener("click", () => {
+      alert("Clock!");
+    });
+    return document.querySelector("button").removeEventListener("click", () => {
+      alert("Clock!");
+    });
+  }, [url]);
   function onWrite(e) {
+    console.log("RENDER");
     setValue(e.target.value);
   }
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((data) => {
-      setGet(data);
-    });
+  // function onHandlerClick() {
+  //     alert('Click!')
+  //   }  onClick={onHandlerClick}
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
@@ -26,6 +37,7 @@ function App() {
           <li key={it.id}>{it.name}</li>
         ))}
       </ul>
+      <button>Handler of Clicks</button>
     </div>
   );
 }
